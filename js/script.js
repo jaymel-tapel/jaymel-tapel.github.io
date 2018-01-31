c0bb25ff50fa23b094f2bb948df88b9adc678e9e
Jaymel Tapel <jaymel.tapel@gmail.com>
2018-01-31 17:35:10 +0800

    testing device throttle

--- js/script.js
+++ js/script.js
@@ -191,17 +191,17 @@ gn.init( args ).then(function(){
 
 
 $window.on('load', function () {
-  $('#hero-slider').slick({
-    dots: true,
-    autoplay: false,
-    speed: 300
-  });  
-  // $('#review-slider').slick({
-  //   prevArrow: '<i class="fa fa-angle-left prev" aria-hidden="true"></i>',
-  //   nextArrow: '<i class="fa fa-angle-right next" aria-hidden="true"></i>',
+  // $('#hero-slider').slick({
   //   dots: true,
-  //   autoplay: true
-  // });
+  //   autoplay: false,
+  //   speed: 300
+  // });  
+  $('#review-slider').slick({
+    prevArrow: '<i class="fa fa-angle-left prev" aria-hidden="true"></i>',
+    nextArrow: '<i class="fa fa-angle-right next" aria-hidden="true"></i>',
+    dots: true,
+    autoplay: true
+  });
   // windowPosition = $(window).scrollTop()+$(window).height();  
   // fitPhotos();
 });
